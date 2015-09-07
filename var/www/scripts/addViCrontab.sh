1aee4720a799abd4be994ab1a5d5ee4c13f18dc4
Raphaël SCHITZ <raphael@schitz.net>
2015-09-08 00:11:48 +0200

    Update addViCrontab.sh

--- var/www/scripts/addViCrontab.sh
+++ var/www/scripts/addViCrontab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sessionFile="/tmp/vpx_$(sed s/\\./_/g <<<$1).dat"
 crontabFile="/etc/cron.d/vi_$(sed s/\\./_/g <<<$1)"
-echo "*/5  *    * * *   root   /usr/bin/perl /root/ViPullStatistics.pl --server $1 --sessionfile $sessionFile" > $crontabFile
+echo "*/5  *    * * *   root   /usr/bin/perl /root/ViPullStatistics.pl --server $1 --sessionfile $sessionFile" >> $crontabFile
