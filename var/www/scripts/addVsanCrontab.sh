7c359400d09dd7a1ff21389d6b24259013239a51
Raphaël SCHITZ <raphael@schitz.net>
2015-09-08 00:11:56 +0200

    Update addVsanCrontab.sh

--- var/www/scripts/addVsanCrontab.sh
+++ var/www/scripts/addVsanCrontab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sessionFile="/tmp/vpx_$(sed s/\\./_/g <<<$1).dat"
 crontabFile="/etc/cron.d/vsan_$(sed s/\\./_/g <<<$1)"
-echo "*  *    * * *   root   /usr/bin/perl /root/VsanPullStatistics.pl --server $1 --sessionfile $sessionFile" > $crontabFile
+echo "*  *    * * *   root   /usr/bin/perl /root/VsanPullStatistics.pl --server $1 --sessionfile $sessionFile" >> $crontabFile
