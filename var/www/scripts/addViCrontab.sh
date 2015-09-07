3696e8450ba4a25f3bbf922d37fbf1c69afa7795
Frederic MARTIN <frederic@martin.lc>
2015-09-07 17:58:52 +0200

    Update addViCrontab.sh

--- var/www/scripts/addViCrontab.sh
+++ var/www/scripts/addViCrontab.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 sessionFile="/tmp/vpx_$(sed s/\\./_/g <<<$1).dat"
-crontabFile="/etc/cron.d2/vi_$(sed s/\\./_/g <<<$1)"
-# /tmp/vpx_184_7_64_240.dat
-#echo "*/5  *    * * *   root   /root/ViPullStatistics.pl --server $1 --sessionfile /tmp/$sessionFile" > $crontabFile
-#echo "*/5  *    * * *   root   /root/ViPullStatistics.pl --server $1 --sessionfile $sessionFile > $crontabFile"
-echo "*/5  *    * * *   root   /root/ViPullStatistics.pl --server $1 --sessionfile $sessionFile" > $crontabFile
+crontabFile="/etc/cron.d/vi_$(sed s/\\./_/g <<<$1)"
+echo "*/5  *    * * *   root   /usr/bin/perl /root/ViPullStatistics.pl --server $1 --sessionfile $sessionFile" > $crontabFile
