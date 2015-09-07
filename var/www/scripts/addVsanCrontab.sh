ef3c66ca816eff3ae09897e84d1d425fc6fb3991
Frederic MARTIN <frederic@martin.lc>
2015-09-07 17:59:12 +0200

    Update addVsanCrontab.sh

--- var/www/scripts/addVsanCrontab.sh
+++ var/www/scripts/addVsanCrontab.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 sessionFile="/tmp/vpx_$(sed s/\\./_/g <<<$1).dat"
-crontabFile="/etc/cron.d2/vsan_$(sed s/\\./_/g <<<$1)"
-# /tmp/vpx_184_7_64_240.dat
-#echo "*/5  *    * * *   root   /root/ViPullStatistics.pl --server $1 --sessionfile /tmp/$sessionFile" > $crontabFile
-#echo "*/5  *    * * *   root   /root/ViPullStatistics.pl --server $1 --sessionfile $sessionFile > $crontabFile"
-echo "*  *    * * *   root   /root/VsanPullStatistics.pl --server $1 --sessionfile $sessionFile" > $crontabFile
+crontabFile="/etc/cron.d/vsan_$(sed s/\\./_/g <<<$1)"
+echo "*  *    * * *   root   /usr/bin/perl /root/VsanPullStatistics.pl --server $1 --sessionfile $sessionFile" > $crontabFile
