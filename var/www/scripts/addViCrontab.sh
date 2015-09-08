4669f0d83932bcfa21cac531ca23c1fcaf565acb
Raphaël SCHITZ <raphael@schitz.net>
2015-09-08 09:23:34 +0200

    Update addViCrontab.sh

--- var/www/scripts/addViCrontab.sh
+++ var/www/scripts/addViCrontab.sh
@@ -2,3 +2,4 @@
 sessionFile="/tmp/vpx_$(sed s/\\./_/g <<<$1).dat"
 crontabFile="/etc/cron.d/vi_$(sed s/\\./_/g <<<$1)"
 echo "*/5  *    * * *   root   /usr/bin/perl /root/ViPullStatistics.pl --server $1 --sessionfile $sessionFile" >> $crontabFile
+service cron reload
