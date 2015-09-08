fd36112af9ab27b8c0506aa496fecc7238d4dd87
Frederic MARTIN <frederic@martin.lc>
2015-09-08 11:18:32 +0200

    Update updateRunner.php

--- var/www/admin/updateRunner.php
+++ var/www/admin/updateRunner.php
@@ -3,7 +3,7 @@ session_start();
 $title = "SexiGraf Package Update Runner";
 require("header.php");
 require("helper.php");
-$dir = '/usr/share/nginx/www/files/';
+$dir = '/var/www/admin/files/';
 $SexiGrafVersion = (file_exists('/etc/sexigraf_version') ? file_get_contents('/etc/sexigraf_version', FILE_USE_INCLUDE_PATH) : "Unknown");
 ?>
     <div class="container"><br/>
@@ -30,39 +30,18 @@ $SexiGrafVersion = (file_exists('/etc/sexigraf_version') ? file_get_contents('/e
                 switch ($_POST["submit"]) {
                         case "upgrade-confirmed":
 			echo "<pre>";
-			#$tmpFolder = trim(shell_exec('mktemp -d'));
 			unlinkRecursive("/tmp/sexigraf-update/");
 			echo "Unpacking SexiGraf Update Package in /tmp/sexigraf-update/\n";
 			echo shell_exec("/usr/bin/unzip \"".$dir.$_POST['input-file']."\" -d /tmp/sexigraf-update/ 2>&1");
 			if (file_exists("/tmp/sexigraf-update/sexigraf-master/updateRunner.sh")) {
-				#echo "Updating Logstash configuration\n";
-				#unlinkRecursive("/etc/logstash/conf.d/");
-				#rcopy("$tmpFolder/logstash","/etc/logstash");
-				#echo "Updating Riemann configuration\n";
-				#unlinkRecursive("/etc/riemann/");
-				#rcopy("$tmpFolder/riemann","/etc/riemann");
-				#echo "Updating SexiGraf version\n";
-				#copy("$tmpFolder/sexigraf_version","/etc/sexigraf_version");
-				#echo "Updating SexiMenu configuration\n";
-				#rcopy("$tmpFolder/seximenu","/root/seximenu");
-				#echo "Press Enter to go on process and restart SexiGraf services...\n";
-				#echo shell_exec("/etc/init.d/riemann stop");
-				#echo shell_exec("/etc/init.d/logstash stop");
-				#echo shell_exec("/etc/init.d/elasticsearch stop");
-				#echo shell_exec("/etc/init.d/elasticsearch start");
-				#echo shell_exec("/etc/init.d/riemann start");
-				#echo shell_exec("/etc/init.d/logstash start");
-				#echo shell_exec("/etc/init.d/node-app restart --force");
-				#echo shell_exec("/etc/init.d/rsyslog restart");
 				$SexiGrafVersion = (file_exists('/etc/sexigraf_version') ? file_get_contents('/etc/sexigraf_version', FILE_USE_INCLUDE_PATH) : "Unknown");
-				#echo 'Current version of SexiGraf is: '.$SexiGrafVersion.'<br />';
 				chmod("/tmp/sexigraf-update/sexigraf-master/updateRunner.sh", 0755);
 				echo shell_exec("sudo /tmp/sexigraf-update/sexigraf-master/updateRunner.sh");
 			} else {
 				echo "!!! Missing mandatory file. Please check package integrity.\n";
 			}
 			echo "Purging temporary folder /tmp/sexigraf-update/";
-			#unlinkRecursive("/tmp/sexigraf-update/");
+			unlinkRecursive("/tmp/sexigraf-update/");
 			echo "</pre>";
 		}
 	}
