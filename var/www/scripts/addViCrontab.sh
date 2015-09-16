e2f5f2ff9f9f51bd2bd95cbfee16d5ad448a8bdd
Raphaël SCHITZ <raphael@schitz.net>
2015-09-16 18:08:14 +0200

    Update addViCrontab.sh
    credstore

--- var/www/scripts/addViCrontab.sh
+++ var/www/scripts/addViCrontab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sessionFile="/tmp/vpx_$(sed s/\\./_/g <<<$1).dat"
 crontabFile="/etc/cron.d/vi_$(sed s/\\./_/g <<<$1)"
-echo "*/5  *    * * *   root   /usr/bin/perl /root/ViPullStatistics.pl --server $1 --sessionfile $sessionFile" >> $crontabFile
+echo "*/5  *    * * *   root   /usr/bin/perl /root/ViPullStatistics.pl --credstore /var/www/.vmware/credstore/vicredentials.xml --server $1 --sessionfile $sessionFile" >> $crontabFile
 service cron reload
