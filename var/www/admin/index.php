3cccd5e9545a9e144127da1dfc3cb0ed0bb7517a
Frederic MARTIN <frederic@martin.lc>
2015-09-24 10:58:29 +0200

    Update index.php
    Adding 'Refresh Inventory'

--- var/www/admin/index.php
+++ var/www/admin/index.php
@@ -4,15 +4,15 @@ $title = "SexiGraf summary";
 require("header.php");
 ?>
 	<div class="row" style="margin:0px;padding:30px;">
-                <div class="col-lg-4 col-md-6">
+                <div class="col-lg-3 col-md-6">
                 	<a href="credstore.php">
 				<div class="panel panel-primary">
                         		<div class="panel-heading">
                             			<div class="row">
-                                			<div class="col-xs-6">
+                                			<div class="col-xs-3">
 				                                <i class="glyphicon glyphicon-briefcase" style="font-size: 4em;"></i>
                                 			</div>
- 		                                	<div class="col-xs-6 text-right">
+ 		                                	<div class="col-xs-9 text-right">
                                     				<div class="huge">Credential Store</div>
                                 			</div>
                             			</div>
@@ -24,15 +24,15 @@ require("header.php");
                     		</div>
                         </a>
                 </div>
-                <div class="col-lg-4 col-md-6">
+                <div class="col-lg-3 col-md-6">
                         <a href="updater.php">
                     		<div class="panel panel-green">
                         		<div class="panel-heading">
                             			<div class="row">
-                                			<div class="col-xs-6">
+                                			<div class="col-xs-3">
                                     				<i class="glyphicon glyphicon-hdd" style="font-size: 4em;"></i>
                                 			</div>
-                                			<div class="col-xs-6 text-right">
+                                			<div class="col-xs-9 text-right">
                                     				<div class="huge">Package Updater</div>
                                 			</div>
                             			</div>
@@ -44,21 +44,41 @@ require("header.php");
                     		</div>
                         </a>
                 </div>
-		<div class="col-lg-4 col-md-6">
+		<div class="col-lg-3 col-md-6">
                         <a href="purge.php">
-                                <div class="panel panel-yellow">
+                                <div class="panel panel-red">
                                         <div class="panel-heading">
                                                 <div class="row">
-                                                        <div class="col-xs-6">
+                                                        <div class="col-xs-3">
                                                                 <i class="glyphicon glyphicon-trash" style="font-size: 4em;"></i>
                                                         </div>
-                                                        <div class="col-xs-6 text-right">
+                                                        <div class="col-xs-9 text-right">
                                                                 <div class="huge">Whisper Purge</div>
                                                         </div>
                                                 </div>
                                         </div>
                                         <div class="panel-footer">
-                                                <span class="pull-left">Whisper purge page will let you manage local whisper data file(s). It can be useful to remove from web-ui some corrupted/unwanted files.</span>
+                                                <span class="pull-left">Whisper purge page will let you manage local whisper data *.wsp file(s). It can be useful to remove from web-ui some corrupted and/or unwanted files.</span>
+                                                <div class="clearfix"></div>
+                                        </div>
+                                </div>
+                        </a>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                        <a href="refresh-inventory.php">
+                                <div class="panel panel-yellow">
+                                        <div class="panel-heading">
+                                                <div class="row">
+                                                        <div class="col-xs-3">
+                                                                <i class="glyphicon glyphicon-th-list" style="font-size: 4em;"></i>
+                                                        </div>
+                                                        <div class="col-xs-9 text-right">
+                                                                <div class="huge">Refresh Inventory</div>
+                                                        </div>
+                                                </div>
+                                        </div>
+                                        <div class="panel-footer">
+                                                <span class="pull-left">The Static Offline Inventory is automatically schedule to be updated every 6 hours. If you want to force a refresh, you can use this section to perform update</span>
                                                 <div class="clearfix"></div>
                                         </div>
                                 </div>
