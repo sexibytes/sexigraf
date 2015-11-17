c26d7c37ad850e1eda4d936a91cf69e1c834e3ee
Frederic MARTIN <frederic@martin.lc>
2015-11-17 11:43:58 +0100

    Fix #28
    Adding vcenter session files and old file purge

--- var/www/admin/purge.php
+++ var/www/admin/purge.php
@@ -1,71 +1,88 @@
-<?php 
+<?php
 session_start();
-$title = "Stats Remover";
+$title = "House Cleaner";
 require("header.php");
 require("helper.php");
 ?>
-	<div class="container"><br/>
-		<div class="panel panel-danger">
-			<div class="panel-heading"><h3 class="panel-title">Stats Remover Notes</h3></div>
-                	<div class="panel-body"><ul>
-                        	<li>This page can be used to purge old and/or unwanted whisper data objects.</li>
-				<li style="color:red;"><strong><span class="glyphicon glyphicon-alert" aria-hidden="true"></span> Beware as this operation cannot be undone, so there is a risk of DATA LOSS if you don't know what you're doing. <span class="glyphicon glyphicon-alert" aria-hidden="true"></span></strong></li>
-                        	<li>Please refer to the <a href="http://www.sexigraf.fr/">project website</a> and documentation for more information.</li>
-                    	</ul></div>
-	        </div>
-            	<h2><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> SexiGraf Stats Remover</h2>
-		<div id="purgeLoading" style="display:block;">
-			<span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Loading filesystem...
-		</div>
+        <div class="container"><br/>
+                <div class="panel panel-danger">
+                        <div class="panel-heading"><h3 class="panel-title">House Cleaner Notes</h3></div>
+                        <div class="panel-body"><ul>
+                                <li>This page can be used to purge old and/or unwanted whisper data objects as well as vCenter session files.</li>
+                                <li style="color:red;"><strong><span class="glyphicon glyphicon-alert" aria-hidden="true"></span> Beware as this operation cannot be undone, so there is a risk of DATA LOSS if you don't know what you're doing. <span class="glyphicon glyphicon-alert" aria-hidden="true"></span></strong></li>
+                                <li>Please refer to the <a href="http://www.sexigraf.fr/">project website</a> and documentation for more information.</li>
+                        </ul></div>
+                </div>
+                <h2><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> SexiGraf House Cleaner</h2>
+                <div id="purgeLoading" style="display:block;">
+                        <span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Loading filesystem...
+                </div>
 <?php
-	if ($_SERVER['REQUEST_METHOD'] == 'POST' and (!empty($_POST['file-to-delete']) or !empty($_POST['pathChecked']))) {
-		switch ($_POST["submit"]) {
-			case "purge-files":
-				echo '  <div class="alert alert-warning" role="warning">
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' and (!empty($_POST['file-to-delete']) or !empty($_POST['pathChecked']))) {
+                switch ($_POST["submit"]) {
+                        case "purge-files":
+                                echo '  <div class="alert alert-warning" role="warning">
                 <h4><span class="glyphicon glyphicon-alert" aria-hidden="true"></span>
                 <span class="sr-only">Warning:</span>
                 Confirmation needed!</h4>
                 You are about to delete the following whisper data objects, are you sure about this? We mean, <strong>really sure</strong>?<br />
-	        <form class="form" action="purge.php" method="post">
-		<ul>';
-				foreach($_POST['pathChecked'] as $check) { 
-					echo "<li><input type=\"hidden\" name=\"file-to-delete[]\" value=\"$check\">" . $check . "</li>\n"; 
-				}
-				echo '</ul>
+                <form class="form" action="purge.php" method="post">
+                <ul>';
+                                foreach($_POST['pathChecked'] as $check) {
+                                        echo "<li><input type=\"hidden\" name=\"file-to-delete[]\" value=\"$check\">" . $check . "</li>\n";
+                                }
+                                echo '</ul>
                         <p><a class="btn btn-success" href="purge.php">Back</a> <button name="submit" class="btn btn-warning" value="purge-files-confirmed">Delete these objects</button></p>
                 </form>';
-				echo '<script type="text/javascript">
-                		        document.getElementById("purgeLoading").style.display = "none";
-                		</script>';
-			break;
-			case "purge-files-confirmed":
-				echo '<script type="text/javascript">
+                                echo '<script type="text/javascript">
                                         document.getElementById("purgeLoading").style.display = "none";
                                 </script>';
-				foreach($_POST['file-to-delete'] as $file2delete) {
-					shell_exec("sudo /bin/bash /var/www/scripts/purgeWhisperFile.sh $file2delete");
-		                }
-				echo '  <div class="alert alert-success" role="success">
+                        break;
+                        case "purge-files-confirmed":
+                                echo '<script type="text/javascript">
+                                        document.getElementById("purgeLoading").style.display = "none";
+                                </script>';
+                                foreach($_POST['file-to-delete'] as $file2delete) {
+                                        shell_exec("sudo /bin/bash /var/www/scripts/purgeWhisperFile.sh $file2delete");
+                                }
+                                echo '  <div class="alert alert-success" role="success">
                 <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
                 <span class="sr-only">Success:</span>
                 Object(s) successfully deleted.<br />
-		<p><a class="btn btn-success" href="purge.php">Back</a></p>
-		</div>';
-			break;
-		}
-	} else {
-		echo '                <div id="purgeTree" style="display:none;">
-		<form action="purge.php" method="post">';
-		echo php_file_tree_dir("/var/lib/graphite/whisper"); 
-		echo '		<button name="submit" class="btn btn-danger" value="purge-files">Purge</button>
-		</form>
-		</div><br />
-		<script type="text/javascript">
-			document.getElementById("purgeTree").style.display = "block";
-			document.getElementById("purgeLoading").style.display = "none";
-		</script>';
-	}
+                <p><a class="btn btn-success" href="purge.php">Back</a></p>
+                </div>';
+                        break;
+                }
+        } else {
+                $topn = 50;
+                echo '                <div id="purgeTree" style="display:none;">
+                <form action="purge.php" method="post">
+                <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation" class="active"><a href="#whisper" aria-controls="whisper" role="tab" data-toggle="tab">Whisper repository</a></li>
+                        <li role="presentation"><a href="#vcenter" aria-controls="vcenter" role="tab" data-toggle="tab">vCenter session files</a></li>
+                        <li role="presentation"><a href="#oldies" aria-controls="oldies" role="tab" data-toggle="tab">Top ' . $topn . ' oldest whisper files</a></li>
+                </ul>
+                <div class="tab-content" style="padding-top: 10px;">
+                        <div role="tabpanel" class="tab-pane fade in active" id="whisper">
+                        ' . php_file_tree_dir("/var/lib/graphite/whisper") . '
+                        </div>
+                        <div role="tabpanel" class="tab-pane fade" id="vcenter">
+                        ' . php_file_tree("/tmp", "dat") . '
+                        </div>
+                        <div role="tabpanel" class="tab-pane fade" id="oldies">
+                        ' . php_file_tree_top_oldest("/var/lib/graphite/whisper", $topn) . '
+                        </div>
+                </div>
+                <button name="submit" class="btn btn-danger" value="purge-files">Purge</button>
+                </form>
+                </div><br />
+                <script type="text/javascript">
+                        document.getElementById("purgeTree").style.display = "block";
+                        document.getElementById("purgeLoading").style.display = "none";
+                </script>';
+        }
 ?>
-	</div>
+        </div>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>
