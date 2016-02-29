5e21e124f5b7d41f4f516560938dfb215c6348f5
Frederic MARTIN <frederic@martin.lc>
2016-02-29 16:55:32 +0100

    Update updater.php

--- var/www/admin/updater.php
+++ var/www/admin/updater.php
@@ -39,6 +39,29 @@ $dir = "/var/www/admin/files/";
             			</div>
             		</div>
             </form>
+          <script>
+          $(document).on('change', '.btn-file :file', function() {
+      var input = $(this),
+          numFiles = input.get(0).files ? input.get(0).files.length : 1,
+          label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+      input.trigger('fileselect', [numFiles, label]);
+    });
+    
+    $(document).ready( function() {
+        $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+    
+            var input = $(this).parents('.input-group').find(':text'),
+                log = numFiles > 1 ? numFiles + ' files selected' : label;
+    
+            if( input.length ) {
+                input.val(log);
+            } else {
+                if( log ) alert(log);
+            }
+    
+        });
+    });
+          </script>
 <?php
         if ($handle = opendir($dir)) {
                 echo '          <table role="presentation" class="table table-striped"><tbody class="files">';
