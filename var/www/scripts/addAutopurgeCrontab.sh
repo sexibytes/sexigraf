129d8a23e179084f71bc59bf52e30b384daa157c
Frederic MARTIN <frederic@martin.lc>
2017-06-05 13:39:09 +0200

    Switch autopurge to leet time and to 120days

--- var/www/scripts/addAutopurgeCrontab.sh
+++ var/www/scripts/addAutopurgeCrontab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 crontabFile="/etc/cron.d/graphite_autopurge"
-echo "*/5  *    * * *   root   /usr/bin/find -L /var/lib/graphite/whisper/ -type f \( -name '*.wsp' \) -mtime +45 -exec rm {} \; ; /usr/bin/find -L /var/lib/graphite/whisper/ -type d -empty -delete" >> $crontabFile
+echo "37  13    * * *   root   /usr/bin/find -L /var/lib/graphite/whisper/ -type f \( -name '*.wsp' \) -mtime +120 -exec rm {} \; ; /usr/bin/find -L /var/lib/graphite/whisper/ -type d -empty -delete" >> $crontabFile
 service cron reload
