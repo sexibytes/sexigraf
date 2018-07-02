ee48ee5a6ee1e05530fd63e58b3a51c974c9abcd
Frederic MARTIN <frederic@martin.lc>
2018-07-02 17:36:12 +0200

    Update exportSexiGrafBundle.sh

--- var/www/scripts/exportSexiGrafBundle.sh
+++ var/www/scripts/exportSexiGrafBundle.sh
@@ -35,4 +35,4 @@ rm -f /root/sexigraf-dump/conf/vicredentials.conf
 #openssl des3 -d -salt -in /root/sexigraf-dump/conf/vicredentials.conf.ss -out /root/sexigraf-dump/conf/vicredentials.conf2 -pass pass:sexigraf
 
 # create ISO file from export folder
-genisoimage -o /var/www/admin/sexigraf-dump.iso /root/sexigraf-dump
+genisoimage -iso-level 4 -o /var/www/admin/sexigraf-dump.iso /root/sexigraf-dump
