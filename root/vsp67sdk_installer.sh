7200cfafe01bf662dabff5929b4c73c06666eb0a
Raphaël SCHITZ <raphael@schitz.net>
2018-07-05 13:31:16 +0000

    Delete vsp67sdk_installer.sh

--- root/vsp67sdk_installer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-tar -xzf /root/VMware-vSphere-Perl-SDK-6.7.0-8156551.x86_64.tar.gz -C /root/
-sed -i 's/ubuntu/debian/g' /root/vmware-vsphere-cli-distrib/vmware-install.pl
-yes | PAGER=cat /root/vmware-vsphere-cli-distrib/vmware-install.pl default
