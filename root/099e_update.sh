1a19b030cdbb3dbb042d9be7e5dfb9d71eedb91b
Raphaël SCHITZ <raphael@schitz.net>
2018-07-05 22:56:12 +0200

    Add files via upload

---
+++ root/099e_update.sh
@@ -0,0 +1,20 @@
+if grep -i -q "Nova Prospekt" /etc/sexigraf_version; then
+ /bin/cp -rf /tmp/sexigraf-update/sexigraf-master/etc/* /etc/
+ /bin/cp -rf /tmp/sexigraf-update/sexigraf-master/root/* /root/
+ /bin/cp -rf /tmp/sexigraf-update/sexigraf-master/usr/* /usr/
+ /bin/cp -rf /tmp/sexigraf-update/sexigraf-master/var/* /var/
+ /bin/tar -xzf /root/VMware-vSphere-Perl-SDK-6.7.0-8156551.x86_64.tar.gz -C /root
+ /bin/sed -i 's/ubuntu/debian/g' /root/vmware-vsphere-cli-distrib/vmware-install.pl
+ yes | PAGER=/bin/cat /root/vmware-vsphere-cli-distrib/vmware-install.pl default
+ rm -rf /root/vmware-vsphere-cli-distrib
+ rm -f /root/VMware-vSphere-Perl-SDK-6.7.0-8156551.x86_64.tar.gz
+ rm -f /root/099e_update.sh
+else
+ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+ echo "     THIS UPDATE IS NOT SUPPORTED ON YOUR SEXIGRAF VERSION     "
+ echo "            PLEASE UPGRADE TO 0.99d AND TRY AGAIN              "
+ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+ exit 1
+fi
