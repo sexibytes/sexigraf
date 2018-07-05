41d9cda7be1782a3c84526b73a544cf2bf61bd69
Raphaël SCHITZ <raphael@schitz.net>
2018-07-05 14:09:11 +0000

    Update vsp67sdk_installer.sh

--- root/vsp67sdk_installer.sh
+++ root/vsp67sdk_installer.sh
@@ -3,3 +3,5 @@
 /bin/tar -xzf /root/VMware-vSphere-Perl-SDK-6.7.0-8156551.x86_64.tar.gz -C /root
 /bin/sed -i 's/ubuntu/debian/g' /root/vmware-vsphere-cli-distrib/vmware-install.pl
 yes | PAGER=/bin/cat /root/vmware-vsphere-cli-distrib/vmware-install.pl default
+rm -rf /root/vmware-vsphere-cli-distrib
+rm /root/VMware-vSphere-Perl-SDK-6.7.0-8156551.x86_64.tar.gz
