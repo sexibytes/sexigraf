fec2bf431dd8786f8c707a860968cf5cc06f2aae
Hypervisor <raphael@schitz.net>
2020-03-11 16:23:37 +0100

    Update addAutopurgeCrontab.sh

--- var/www/scripts/addAutopurgeCrontab.sh
+++ var/www/scripts/addAutopurgeCrontab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 crontabFile="/etc/cron.d/graphite_autopurge"
-echo "37  13    * * *   root   /usr/bin/find -L /var/lib/graphite/whisper/vmw/ -type f \( -name '*.wsp' \) -mtime +120 -exec rm {} \; ; /usr/bin/find -L /var/lib/graphite/whisper/ -type d -empty -delete" >> $crontabFile
+echo "37  13    * * *   root   /usr/bin/find -L /zfs/whisper/vmw/ -type f \( -name '*.wsp' \) -mtime +120 -exec rm {} \; ; /usr/bin/find -L /zfs/whisper/ -type d -empty -delete" >> $crontabFile
 service cron reload
