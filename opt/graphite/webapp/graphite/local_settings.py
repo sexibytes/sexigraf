36389c96695e85c8dcd6ae13dc619550a15c230e
Hypervisor <raphael@schitz.net>
2020-04-13 22:55:25 +0200

    Update local_settings.py

--- opt/graphite/webapp/graphite/local_settings.py
+++ opt/graphite/webapp/graphite/local_settings.py
@@ -11,7 +11,7 @@
 # install. This key is used for salting of hashes used in auth tokens,
 # CRSF middleware, cookie storage, etc. This should be set identically among
 # instances if used behind a load balancer.
-SECRET_KEY = '85662d6f52bca67db0474ae002527773'
+SECRET_KEY = 'Sex1Gr4f'
 
 # In Django 1.5+ set this to the list of hosts your graphite instances is
 # accessible as. See:
