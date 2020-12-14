2041c6066e127424cc2cfd15077aa07b3d96efdf
Raphaël SCHITZ <raphael@schitz.net>
2020-12-14 20:52:34 +0100

    Update VsanPullStatistics.ps1

--- opt/sexigraf/VsanPullStatistics.ps1
+++ opt/sexigraf/VsanPullStatistics.ps1
@@ -6,7 +6,7 @@ $ExecStart = Get-Date
 $ScriptVersion = "0.9.1"
 
 try {
-    Start-Transcript -Path "/var/log/sexigraf/ViPullStatistics.$($server).log" -Append -Confirm:$false -Force
+    Start-Transcript -Path "/var/log/sexigraf/VsanDisksPullStatistics.$($server).log" -Append -Confirm:$false -Force
     Write-Host "$((Get-Date).ToString("o")) [DEBUG] VsanDisksPullStatistics v$ScriptVersion"
 } catch {
     Write-Host "$((Get-Date).ToString("o")) [ERROR] VsanDisksPullStatistics logging failure"
@@ -17,7 +17,7 @@ if ($SessionFile) {
     try {
         $SessionToken = (Get-Content -Path $SessionFile -Force -Delimiter '\"')[1]
         Write-Host "$((Get-Date).ToString("o")) [INFO] SessionToken found in SessionFile, attempting connection to $Server"
-        $PowerCliConfig = Set-PowerCLIConfiguration -ProxyPolicy NoProxy -DefaultVIServerMode Single -InvalidCertificateAction Ignore -ParticipateInCeip:$false -DisplayDeprecationWarnings:$false -Confirm:$false -Scope Session
+        # $PowerCliConfig = Set-PowerCLIConfiguration -ProxyPolicy NoProxy -DefaultVIServerMode Single -InvalidCertificateAction Ignore -ParticipateInCeip:$false -DisplayDeprecationWarnings:$false -Confirm:$false -Scope Session
         $ServerConnection = Connect-VIServer -Server $Server -Session $SessionToken -Force
         if ($ServerConnection.IsConnected) {
             Write-Host "$((Get-Date).ToString("o")) [INFO] Connected to vCenter $($ServerConnection.Name) version $($ServerConnection.Version) build $($ServerConnection.Build)"
