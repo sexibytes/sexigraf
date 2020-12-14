9ee8cf2921a8dbb4ed7681f29374fd87a91f1e96
Raphaël SCHITZ <raphael@schitz.net>
2020-12-14 13:32:59 +0100

    Update VsanPullStatistics.ps1

--- opt/sexigraf/VsanPullStatistics.ps1
+++ opt/sexigraf/VsanPullStatistics.ps1
@@ -5,3 +5,10 @@ param([Parameter (Mandatory=$true)] [string] $server, [Parameter (Mandatory=$tru
 $exec_start = Get-Date
 $script_version = "0.9.1"
 
+try {
+    Start-Transcript -Path "/var/log/sexigraf/ViPullStatistics.$($server).log" -Append -Confirm:$false -Force
+    Write-Host "$((Get-Date).ToString("o")) [DEBUG] VsanDisksPullStatistics v$script_version"
+} catch {
+    Write-Host "$((Get-Date).ToString("o")) [ERROR] VsanDisksPullStatistics logging failure"
+    exit
+}
