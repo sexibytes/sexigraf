a4369ce4d5028b1b4b1c69682e3b271a5c84c0ae
Raphaël SCHITZ <raphael@schitz.net>
2020-12-14 13:38:03 +0100

    Update VsanPullStatistics.ps1

--- opt/sexigraf/VsanPullStatistics.ps1
+++ opt/sexigraf/VsanPullStatistics.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/pwsh
 #
-param([Parameter (Mandatory=$true)] [string] $server, [Parameter (Mandatory=$true)] [Parameter (Mandatory=$false)] [string] $sessionfile, [Parameter (Mandatory=$false)] [string] $credstore)
+param([Parameter (Mandatory=$true)] [string] $server, [Parameter (Mandatory=$true)] [string] $sessionfile, [Parameter (Mandatory=$false)] [string] $credstore)
 
 $exec_start = Get-Date
 $script_version = "0.9.1"
