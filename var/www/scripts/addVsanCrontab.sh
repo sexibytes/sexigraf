35393577a95ca1e5346ef7b6cfba758ae5b5c604
Raphaël SCHITZ <raphael@schitz.net>
2021-04-23 11:28:46 +0200

    Update addVsanCrontab.sh
    #254

--- var/www/scripts/addVsanCrontab.sh
+++ var/www/scripts/addVsanCrontab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sessionFile="/tmp/vpx_$(sed s/\\./_/g <<<$1).dat"
+sessionFile="/tmp/vmw_$(sed s/\\./_/g <<<$1).key"
 crontabFile="/etc/cron.d/vsan_$(sed s/\\./_/g <<<$1)"
 echo "*  *    * * *   root   /usr/bin/pwsh -f  /opt/sexigraf/VsanPullStatistics.ps1 -credstore /var/www/.vmware/credstore/vicredentials.xml -server $1 -sessionfile $sessionFile >/dev/null 2>&1" >> $crontabFile
 service cron reload
