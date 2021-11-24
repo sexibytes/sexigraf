7bf4356bff978b3f06e08709ba56642bed207217
Raphaël SCHITZ <raphael@schitz.net>
2021-11-24 13:29:39 +0100

    Update updateInventory.sh

--- var/www/scripts/updateInventory.sh
+++ var/www/scripts/updateInventory.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/pwsh -NonInteractive -NoProfile -f /opt/sexigraf/ViOfflineInventory.ps1 -credstore /var/www/.vmware/credstore/vipscredentials.xml
+/usr/bin/pwsh -NonInteractive -NoProfile -f /opt/sexigraf/ViOfflineInventory.ps1 -credstore /var/www/.vmware/credstore/vipscredentials.xml >/dev/null 2>&1
