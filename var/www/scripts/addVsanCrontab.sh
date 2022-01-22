60a808168195d23dddfdfb151bed24580829488e
Raphaël SCHITZ <raphael@schitz.net>
2022-01-23 00:18:33 +0100

    Update addVsanCrontab.sh

--- var/www/scripts/addVsanCrontab.sh
+++ var/www/scripts/addVsanCrontab.sh
@@ -3,3 +3,4 @@ sessionFile="/tmp/vmw_$(sed s/\\./_/g <<<$1).key"
 crontabFile="/etc/cron.d/vsan_$(sed s/\\./_/g <<<$1)"
 echo "*  *    * * *   root   /usr/bin/pwsh -NonInteractive -NoProfile -f  /opt/sexigraf/VsanPullStatistics.ps1 -credstore /mnt/wfs/inventory/vipscredentials.xml -server $1 -sessionfile $sessionFile >/dev/null 2>&1" >> $crontabFile
 service cron reload
+# Virtual SAN has left the building
