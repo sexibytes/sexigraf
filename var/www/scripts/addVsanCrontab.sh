2a8280e032cc9569ff34be9c253b7fc68684e8b7
Hypervisor <raphael@schitz.net>
2022-02-04 15:37:50 +0100

    Update addVsanCrontab.sh
    #264

--- var/www/scripts/addVsanCrontab.sh
+++ var/www/scripts/addVsanCrontab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sessionFile="/tmp/vmw_$(sed s/\\./_/g <<<$1).key"
+# sessionFile="/tmp/vmw_$(sed s/\\./_/g <<<$1).key"
 crontabFile="/etc/cron.d/vsan_$(sed s/\\./_/g <<<$1)"
-echo "*  *    * * *   root   /usr/bin/pwsh -NonInteractive -NoProfile -f  /opt/sexigraf/VsanPullStatistics.ps1 -credstore /mnt/wfs/inventory/vipscredentials.xml -server $1 -sessionfile $sessionFile >/dev/null 2>&1" >> $crontabFile
+# echo "*  *    * * *   root   /usr/bin/pwsh -NonInteractive -NoProfile -f  /opt/sexigraf/VsanPullStatistics.ps1 -credstore /mnt/wfs/inventory/vipscredentials.xml -server $1 -sessionfile $sessionFile >/dev/null 2>&1" >> $crontabFile
+echo "# Virtual SAN has left the building" >> $crontabFile
 service cron reload
-# Virtual SAN has left the building
