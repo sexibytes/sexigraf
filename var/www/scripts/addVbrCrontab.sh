e90d6355d5eb5b3456aca0c3ba47de7bd61429f4
Raphaël SCHITZ <raphael@schitz.net>
2023-05-24 00:57:48 +0200

    Create addVbrCrontab.sh
    #147

---
+++ var/www/scripts/addVbrCrontab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sessionFile="/tmp/vbr_$(sed s/\\./_/g <<<$1).key"
+crontabFile="/etc/cron.d/vbr_$(sed s/\\./_/g <<<$1)"
+echo "*/5  *    * * *   root   /usr/bin/pwsh -NonInteractive -NoProfile -f  /opt/veeam/VbrPullStatistics.ps1 -credstore /mnt/wfs/inventory/vbrpscredentials.xml -server $1 -sessionfile $sessionFile >/dev/null 2>&1" >> $crontabFile
+service cron reload
