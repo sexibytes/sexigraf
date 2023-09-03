0ee9dcd99cf41c5eeb4d2740edad77030e7a1393
Raphaël SCHITZ <raphael@schitz.net>
2023-09-03 23:38:07 +0200

    Update addVbrCrontab.sh
    #147

--- var/www/scripts/addVbrCrontab.sh
+++ var/www/scripts/addVbrCrontab.sh
@@ -2,4 +2,5 @@
 sessionFile="/tmp/vbr_$(sed s/\\./_/g <<<$1).key"
 crontabFile="/etc/cron.d/vbr_$(sed s/\\./_/g <<<$1)"
 echo "*/5  *    * * *   root   /usr/bin/pwsh -NonInteractive -NoProfile -f  /opt/veeam/VbrPullStatistics.ps1 -credstore /mnt/wfs/inventory/vbrpscredentials.xml -server $1 -sessionfile $sessionFile >/dev/null 2>&1" >> $crontabFile
+echo "3  */1    * * *   root /usr/bin/pwsh -NonInteractive -NoProfile -f /opt/veeam/VbrVmInventory.ps1 >/dev/null 2>&1" > "/etc/cron.d/VbrVmInventory"
 service cron reload
