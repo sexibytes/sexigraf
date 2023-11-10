a0baf51917fe91f9bf1a5ad11ea47b758396bdae
Raphaël SCHITZ <raphael@schitz.net>
2023-11-11 00:04:57 +0100

    Update VbrPullStatistics.ps1
    275

--- opt/veeam/VbrPullStatistics.ps1
+++ opt/veeam/VbrPullStatistics.ps1
@@ -2,7 +2,7 @@
 #
 param([Parameter (Mandatory=$true)] [string] $Server, [Parameter (Mandatory=$true)] [string] $SessionFile, [Parameter (Mandatory=$false)] [string] $CredStore)
 
-$ScriptVersion = "0.9.38"
+$ScriptVersion = "0.9.39"
 
 $ExecStart = $(Get-Date).ToUniversalTime()
 # $stopwatch =  [system.diagnostics.stopwatch]::StartNew()
@@ -402,7 +402,7 @@ if ($($VbrJobsStates.data)) {
                         SexiLogger "[EROR] $($Error[0])"
                     }
 
-                    if ($(Get-ChildItem "/mnt/wfs/inventory/VbrVmInventory.*.csv") -and $ExecStart.DayOfWeek -match "Monday" -and $ExecStart.Hour -match "1") {
+                    if ($(Get-ChildItem "/mnt/wfs/inventory/VbrVmInventory.*.csv")) {
                         SexiLogger "[INFO] Rotating VbrVmInventory.*.csv files ..."
                         $ExtraCsvFiles = Compare-Object  $(Get-ChildItem "/mnt/wfs/inventory/VbrVmInventory.*.csv")  $(Get-ChildItem "/mnt/wfs/inventory/VbrVmInventory.*.csv"|Sort-Object LastWriteTime | Select-Object -Last 10) -property Name | ?{$_.SideIndicator -eq "<="}
                         If ($ExtraCsvFiles) {
@@ -418,7 +418,9 @@ if ($($VbrJobsStates.data)) {
                     try {
                         $VbrVmInventoryCsv = Import-Csv $(Get-ChildItem /mnt/wfs/inventory/VbrInv_*.csv).fullname | Sort-Object LastRestorePoint -Descending
                         $VbrVmInventoryCsv | Export-Csv /mnt/wfs/inventory/VbrVmInventory.csv -ErrorAction Stop -Force
-                        $VbrVmInventoryCsv | Export-Csv /mnt/wfs/inventory/VbrVmInventory.$((Get-Date).ToString("yyyy.MM.dd_hh.mm.ss")).csv -ErrorAction Stop -Force
+                        if ($ExecStart.DayOfWeek -match "Monday" -and $ExecStart.Hour -match "1") {
+                            $VbrVmInventoryCsv | Export-Csv /mnt/wfs/inventory/VbrVmInventory.$((Get-Date).ToString("yyyy.MM.dd_hh.mm.ss")).csv -ErrorAction Stop -Force
+                        }
                     } catch {
                         SexiLogger "[EROR] Import/Export VBR inventories issue"
                         SexiLogger "[EROR] $($Error[0])"
