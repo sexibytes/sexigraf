5f7d3700be7e701d77e581f7a123fc8c71420827
Raphaël SCHITZ <raphael@schitz.net>
2026-04-01 23:27:21 +0200

    Update importSexiGrafBundle.sh

--- var/www/scripts/importSexiGrafBundle.sh
+++ var/www/scripts/importSexiGrafBundle.sh
@@ -90,7 +90,7 @@ if [ -d "/mnt/wfs/whisper" ]; then
     apachectl restart
 
     # 0.99m Timepicker bug in grafana 8.5.10 #366 fix 
-    nohup /usr/bin/find -L /mnt/wfs/whisper/vmw/ /mnt/wfs/whisper/esx/ -type f -name '*.wsp' -exec /usr/local/bin/whisper-resize.py {} 5m:25h 10m:49h 30m:97h 60m:8d 240m:31d 720m:91d 2880m:1y 5760m:2y 17280m:5y --nobackup --force \; > /var/log/sexigraf/wsp_resize.log 2>&1 &
+    nohup /usr/bin/find -L /mnt/wfs/whisper/vmw/ /mnt/wfs/whisper/esx/ -type f -name '*.wsp' -exec /usr/local/bin/whisper-resize.py {} 5m:25h 10m:49h 30m:97h 60m:8d 240m:31d 720m:91d 2880m:1y 5760m:2y 17280m:5y --nobackup --aggregate --force \; > /var/log/sexigraf/wsp_resize.log 2>&1 &
 
     # Inventory Update
     nohup /bin/bash /var/www/scripts/updateInventory.sh &
